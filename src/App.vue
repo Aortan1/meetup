ecd6390fb5c7196d3bc9478ddebc56a4c3b018b8
Alexander Demushkin <alexandr.d@powercode.us>
2017-12-25 19:09:30 +0200

    Commit 1.

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app >
+    <v-toolbar>
+
+      <v-toolbar-title> DevMeetup </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left color="green">supervisor_account</v-icon>
+          View Meetups
+        </v-btn>
+
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+/*#app {*/
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+/*}*/
+
+/*h1, h2 {*/
+  /*font-weight: normal;*/
+/*}*/
+
+/*ul {*/
+  /*list-style-type: none;*/
+  /*padding: 0;*/
+/*}*/
+
+/*li {*/
+  /*display: inline-block;*/
+  /*margin: 0 10px;*/
+/*}*/
+
+/*a {*/
+  /*color: #42b983;*/
+/*}*/
+</style>
